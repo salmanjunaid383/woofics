c6b32e6f6343d2cb086b8cac5c91c1d28acf83ef
unknown <muhammad.aahad81@gmail.com>
2021-07-29 21:46:49 +0500

    added price field

--- src/Components/SuperPortal/Stazbar.js
+++ src/Components/SuperPortal/Stazbar.js
@@ -229,11 +229,11 @@ export default function StazBar() {
             icon: <RateReviewIcon style={{ color: "#8da2b1" }} />,
             to: '/viewreviews'
         },
-        {
-            name: 'Coupons',
-            icon: <LocalOfferIcon style={{ color: "#8da2b1" }} />,
-            to: '/couponslist'
-        },
+        // {
+        //     name: 'Coupons',
+        //     icon: <LocalOfferIcon style={{ color: "#8da2b1" }} />,
+        //     to: '/couponslist'
+        // },
         {
             name: 'Led ',
             icon: <DvrIcon style={{ color: "#8da2b1" }} />,
