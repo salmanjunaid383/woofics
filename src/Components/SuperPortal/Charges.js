d6d838b70c16ba9718b3f39f203b25607b1e7c89
unknown <muhammad.aahad81@gmail.com>
2021-07-29 22:58:02 +0500

    remove charges

--- src/Components/SuperPortal/Charges.js
+++ src/Components/SuperPortal/Charges.js
@@ -34,10 +34,10 @@ export default function Charges() {
                     <div className="page-wrapper bg-light">
                         <div class="container pb-lg-4">
                             <div className="row m-lg-5 text-center mx-auto my-auto">
-                                <div className="col-md-5 m-3 p-5 mx-auto text-center shadow border rounded" style={{ cursor: 'pointer' }} onClick={() => history.push('/createforms/value')}>
+                                {/* <div className="col-md-5 m-3 p-5 mx-auto text-center shadow border rounded" style={{ cursor: 'pointer' }} onClick={() => history.push('/createforms/value')}>
                                     <i className="fa fa-5x py-3 fa-money " style={{ color: "#f25c8a" }}></i>
                                     <h5 className="h5">Cargo Por Valor</h5>
-                                </div>
+                                </div> */}
                                 <div className="col-md-5 m-3 p-5 mx-auto text-center shadow border rounded" style={{ cursor: 'pointer' }} onClick={() => history.push('/createforms/supplier')}>
                                     <i className="fa fa-5x py-3 fa-cubes " style={{ color: "#9b88f6" }}></i>
                                     <h5 className="h5">Paquetes De Proveedores</h5>
